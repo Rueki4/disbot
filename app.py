382738c99e75bea7982a1a80964ae7c58a45668f
Luis Tulis <luistuliscurra@gmail.com>
2025-06-21 05:56:13 -0300

    epico

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands, tasks
 from bs4 import BeautifulSoup
 import requests
-
+import funciones
 
 index = -1
 intents = discord.Intents.all()
@@ -11,21 +11,34 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="'", intents=intents)
 
+lista_auto = []
+
 @bot.event
 async def on_ready():
     print(f"Conectado como {bot.user}")
-    enviar_mensaje_periodico.start()  # Inicia el loop cuando el bot esté listo
+    lista_automatica.start() 
 
 @tasks.loop(minutes = 1)
-async def enviar_mensaje_periodico():
-    canal = bot.get_channel(1385816218707038268)
-    print(canal)
-    if canal:
-        await canal.send("holaaaaa")
+async def lista_automatica():
+    for i in range(len(lista_auto)):
+        content, canal_id = lista_auto[i]()
+        canal = bot.get_channel(canal_id)
+        if canal:
+            await canal.send(content)
 
 @bot.command()
-async def info(ctx, nombre : str):
-    await ctx.send(nombre)
+async def randomrule(ctx, tag : str = "", score : int = 0):
+    global lista_auto
+    lista_auto.append(funciones.crear_randomizador(tag, ctx.channel.id, score))
+
+@bot.command()
+async def rule(ctx, tag : str, score : int = 0):
+
+    await ctx.send(funciones.random_de(tag, ctx.channel.id, score))
+
+@bot.command()
+async def info(ctx):
+    await ctx.send('A bot just for u ♥, By Rueki')
 
 @bot.command()
 async def intento(ctx, nombre : str):
@@ -35,31 +48,29 @@ async def intento(ctx, nombre : str):
 async def on_message(message):
     if message.author == bot.user:
         return
-    # global index
-    if 'hola' in message.content.lower():
+    if 'jarracamalachu' in message.content.lower():
 
-        # url = "https://rule34.xxx/index.php?page=post&s=list&tags=futabu+"
-        # headers = {
-        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-        # }
-        # hentai_list = []
-        # resp = requests.get(url, headers=headers)
-        # # resp.raise_for_status()  # lanza error si el status ≠ 200
-        # soup = BeautifulSoup(resp.text, "html.parser")
+        url = "https://rule34.xxx/index.php?page=post&s=list&tags=futabu+"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        }
+        hentai_list = []
+        resp = requests.get(url, headers=headers)
+        # resp.raise_for_status()  # lanza error si el status ≠ 200
+        soup = BeautifulSoup(resp.text, "html.parser")
 
-        # # Ejemplo: títulos y enlaces de resultados
-        # soup_str = str(soup).split("\n")
+        # Ejemplo: títulos y enlaces de resultados
+        soup_str = str(soup).split("\n")
 
-        # for i in range(len(soup_str)):
-        #     # print(soup_str[i])
-        #     if "thumbnails" in soup_str[i]:
-        #         source = soup_str[i].split('src')[1].split('"')[1]
-        #         hentai_list.append(source)
-        # index += 1
+        for i in range(len(soup_str)):
+            print(soup_str[i])
+            if "thumbnails" in soup_str[i]:
+                source = soup_str[i].split('src')[1].split('"')[1]
+                hentai_list.append(source)
+        index += 1
         await message.channel.send("hola")
 
     await bot.process_commands(message)
-
 
 token_a = "MTM4NTc3NTU2MDA2MzQ1MTIyNw."
 token_b = "GzW8GX.6gHVgapzMm8L40X8LQpasXj1wP8iLU7mVRfX50"
---
+++ funciones.py
@@ -0,0 +1,84 @@
+import discord
+from discord.ext import commands, tasks
+from bs4 import BeautifulSoup
+import requests
+import random
+
+def crear_randomizador(tag : str, canal : str) -> str:
+    def nueva_funcion():
+        try:
+            url = "https://rule34.xxx/index.php?page=post&s=list&tags=" + tag + "+&pid=0"
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+            }
+
+            resp = requests.get(url, headers=headers)
+            soup = BeautifulSoup(resp.text, "html.parser")
+
+            soup_str = str(soup).split("\n")
+            indice_pass = 0
+            for i in range(len(soup_str)):
+                if ";pid=" in soup_str[i]:
+                    last_pid = soup_str[i].split('pid=')[-1].split('"')[0]
+                    pid = random.randint(0, int(last_pid))
+                    url = f"{url[0:-1]}{pid}"
+                    
+                    resp = requests.get(url, headers=headers)
+                    soup = BeautifulSoup(resp.text, "html.parser")
+
+                    new_soup_str = str(soup).split("\n")
+                    for j in range(len(new_soup_str)):
+                        if "score" in new_soup_str[j]:
+                            if int(new_soup_str[j].split("score:")[1].split(" ")[0]) > 100:
+                                code = new_soup_str[j].split("?")[1].split('"')[0]
+                                break
+                    url = f"https://rule34.xxx/index.php?page=post&s=view&id={code}"
+                    resp = requests.get(url, headers=headers)
+                    soup = BeautifulSoup(resp.text, "html.parser")
+
+                    last_soup_str = "https://wimg." + str(soup).split("https://wimg.")[1].split('"')[0]
+        except:
+            last_soup_str = "Enjoying the goon sesion?"
+        return last_soup_str, canal
+    print("De momento todo bien")
+    return nueva_funcion
+
+def random_de(tag : str, canal : str, score : int = 0) -> str:
+    try:
+        url = "https://rule34.xxx/index.php?page=post&s=list&tags=" + tag + "+&pid=0"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        }
+
+        resp = requests.get(url, headers=headers)
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        soup_str = str(soup).split("\n")
+        indice_pass = 0
+        for i in range(len(soup_str)):
+            if ";pid=" in soup_str[i]:
+                last_pid = soup_str[i].split('pid=')[-1].split('"')[0]
+                pid = random.randint(0, int(last_pid))
+                url = f"{url[0:-1]}{pid}"
+                
+                resp = requests.get(url, headers=headers)
+                soup = BeautifulSoup(resp.text, "html.parser")
+
+                new_soup_str = str(soup).split("\n")
+                for j in range(len(new_soup_str)):
+                    if "score" in new_soup_str[j]:
+                        if int(new_soup_str[j].split("score:")[1].split(" ")[0]) > score:
+                            code = new_soup_str[j].split("?")[1].split('"')[0]
+                            break
+                url = f"https://rule34.xxx/index.php?page=post&s=view&id={code}"
+                resp = requests.get(url, headers=headers)
+                soup = BeautifulSoup(resp.text, "html.parser")
+
+                last_soup_str = "https://wimg." + str(soup).split("https://wimg.")[1].split('"')[0]
+    except:
+        last_soup_str = "Enjoying the goon sesion?"
+    return last_soup_str
+
+
+
+# print(random_de("bulma_briefs+-ai_generated"))
